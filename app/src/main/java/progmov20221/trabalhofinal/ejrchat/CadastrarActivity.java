37a04c54d753a4d35aa3b39a6f0bbb970d0e9b45
Jeú Chaves Lima <jeuchaveslima@gmail.com>
2022-07-01 17:56:31 -0400

    Firebase: Criando função de cadastros de usuarios [Testado]

--- app/src/main/java/progmov20221/trabalhofinal/ejrchat/CadastrarActivity.java
+++ app/src/main/java/progmov20221/trabalhofinal/ejrchat/CadastrarActivity.java
@@ -1,14 +1,70 @@
 package progmov20221.trabalhofinal.ejrchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CadastrarActivity extends AppCompatActivity {
+
+    private EditText edt_usuario;
+    private EditText edt_email;
+    private EditText edt_senha;
+    private Button btn_cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
+
+        edt_usuario = findViewById(R.id.edt_usuario_cadastro);
+        edt_email = findViewById(R.id.edt_email_cadastro);
+        edt_senha = findViewById(R.id.edt_senha_cadastro);
+        btn_cadastrar = findViewById(R.id.btn_cadastrar);
+
+        btn_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                criarUsuario();
+            }
+        });
+    }
+
+    private void criarUsuario() {
+        String email = edt_email.getText().toString();
+        String senha = edt_senha.getText().toString();
+
+        if(email == null || senha == null || email.isEmpty() || senha.isEmpty()) {
+            Toast.makeText(this, "Senha e email devem ser preenchidos", Toast.LENGTH_SHORT).show();
+            Log.i("Teste", "Senha e email devem ser preenchidos");
+            return;
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                            Log.i("Teste", task.getResult().getUser().getUid());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("Teste", e.getMessage());
+                    }
+                });
     }
 }
