4633a44b224c678f1fd3b1cb59fa1cf1c3ea17e3
Jeú Chaves Lima <jeuchaveslima@gmail.com>
2022-07-01 19:51:30 -0400

    Configuração do menu de navegação e adição da função sair

--- app/src/main/java/progmov20221/trabalhofinal/ejrchat/MensagensActivity.java
+++ app/src/main/java/progmov20221/trabalhofinal/ejrchat/MensagensActivity.java
@@ -1,10 +1,14 @@
 package progmov20221.trabalhofinal.ejrchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,5 +28,25 @@ public class MensagensActivity extends AppCompatActivity {
             it.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(it);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.contatos:
+
+                break;
+            case R.id.sair:
+                FirebaseAuth.getInstance().signOut();
+                verificarAutenticacao();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
