0d511911191954fc118a4ddd96f6177888f8f822
Jeú Chaves Lima <jeuchaveslima@gmail.com>
2022-07-02 16:48:20 -0400

    Mostrar todos os usuarios presente no firebase

--- app/src/main/java/progmov20221/trabalhofinal/ejrchat/ContatosActivity.java
+++ app/src/main/java/progmov20221/trabalhofinal/ejrchat/ContatosActivity.java
@@ -1,16 +1,23 @@
 package progmov20221.trabalhofinal.ejrchat;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.GroupieViewHolder;
+import com.xwray.groupie.Item;
 
 import java.util.List;
 
@@ -18,11 +25,18 @@ import progmov20221.trabalhofinal.ejrchat.model.Usuario;
 
 public class ContatosActivity extends AppCompatActivity {
 
+    private GroupAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contatos);
-        
+
+        RecyclerView rv = findViewById(R.id.recycler);
+        adapter = new GroupAdapter();
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
         buscarUsuarios();
     }
 
@@ -39,9 +53,30 @@ public class ContatosActivity extends AppCompatActivity {
                         List<DocumentSnapshot> docs = value.getDocuments();
                         for(DocumentSnapshot doc : docs) {
                             Usuario usuario = doc.toObject(Usuario.class);
+                            adapter.add(new UsuarioRecycler(usuario));
                             Log.d("Teste", usuario.getNome());
                         }
                     }
                 });
+    }
+
+    private class UsuarioRecycler extends Item {
+
+        private final Usuario usuario;
+
+        private UsuarioRecycler(Usuario usuario) {
+            this.usuario = usuario;
+        }
+
+        @Override
+        public void bind(@NonNull GroupieViewHolder viewHolder, int position) {
+            TextView txt_nome_usuario = viewHolder.itemView.findViewById(R.id.txt_nome_usuario);
+            txt_nome_usuario.setText(usuario.getNome());
+        }
+
+        @Override
+        public int getLayout() {
+            return R.layout.usuario_recycler;
+        }
     }
 }
